ba9f949d5c1445e7b89fd87eaf1c6ea6db393319
kristjangrm <kristjan.grm1@gmail.com>
2021-12-05 17:04:24 +0100

    added types for graphql quires

--- graphql/queries.ts
+++ graphql/queries.ts
@@ -31,3 +31,22 @@ query {
   }
 }
 `;
+
+export type RecommendedAccount = {
+  address: string,
+  ens: string,
+  followerCount: string,
+  recommendationReason: string,
+  displayName: string
+};
+
+
+export type PopularAccount = {
+  address : string,
+  ens : string,
+  followerCount : string,
+  followStatus : {
+    isFollowed : boolean,
+    isFollowing : boolean
+  },
+};
