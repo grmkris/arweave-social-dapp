1cb06e027803811d6fd5f4a1d6695a8c7e572df8
kristjangrm <kristjan.grm1@gmail.com>
2021-12-08 20:13:08 +0100

    using proper provider parameter and using own namespace in cyberconnect

--- hooks/useCyberConnect.tsx
+++ hooks/useCyberConnect.tsx
@@ -1,20 +1,22 @@
 import { useEffect, useState } from "react";
-import { useWeb3React } from "@web3-react/core";
+import { useWeb3React, Web3ReactProvider  } from "@web3-react/core";
 import CyberConnect, { Blockchain, Env } from "@cyberlab/cyberconnect";
+import { Web3Provider } from "@ethersproject/providers";
 
 export default function useCyberConnect() {
   const [cyberConnect, setCyberConnect] = useState<CyberConnect | null>(null);
   const [initializing, setInitializing] = useState(true);
   const { library } = useWeb3React();
 
+
   useEffect(() => {
     if (!library) return;
 
     const cyberConnect = new CyberConnect({
-      namespace: "CyberConnect",
+      namespace: "OurSpace",
       env: Env.PRODUCTION,
       chain: Blockchain.ETH,
-      provider: library,
+      provider: library.provider,
     });
     setCyberConnect(cyberConnect);
     setInitializing(false);
